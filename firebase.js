9ab3d2e7d0c230c6b57b6d375a10a917d03789ca
Muhamad Luqman <muhamadluqmanhakim69@gmail.com>
2021-08-17 14:32:34 +0800

    add checkou page

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB0YVo621KHnzKWciY0lsYS5fLsBfsMnBE",
+  authDomain: "clone-c6556.firebaseapp.com",
+  projectId: "clone-c6556",
+  storageBucket: "clone-c6556.appspot.com",
+  messagingSenderId: "133549783168",
+  appId: "1:133549783168:web:8f61aebd512962c1ce0e65"
+};
+
